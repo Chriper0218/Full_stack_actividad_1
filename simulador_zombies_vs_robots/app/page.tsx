0e0dd9f4db8ed87c71991f6accc62433b773e11d
Chriper0218 <140846210+Chriper0218@users.noreply.github.com>
2026-04-25 21:34:28 +0000

    arreglo force dynamic rendering para mostrar nuevos personajes

--- simulador_zombies_vs_robots/app/page.tsx
+++ simulador_zombies_vs_robots/app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma"; 
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   // SSR: Traemos los personajes directo de la base de datos
   const characters = await prisma.character.findMany();
